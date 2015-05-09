73088208289820260a88eae1dde59ef9c3c167b2
mio_k <miokato@me.com>
2015-05-09 18:04:00 +0900

    add 2 samples

---
+++ day5/ArduinoCode/sample02_random/src/sketch.ino
@@ -0,0 +1,206 @@
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define LED4 5
+#define LED5 6
+#define LED6 7
+#define LED7 8
+#define LED8 9
+#define LED9 10
+#define LED10 11
+
+// set blinking speed of the LED
+int spd = 10;
+
+void setup()
+{
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+    pinMode(LED5, OUTPUT); 
+    pinMode(LED6, OUTPUT);
+    pinMode(LED7, OUTPUT);
+    pinMode(LED8, OUTPUT);
+    pinMode(LED9, OUTPUT);
+    pinMode(LED10, OUTPUT); 
+
+    randomSeed(analogRead(0));
+}
+
+void loop()
+{
+    randomBlinking();
+    delay(spd);
+}
+
+void randomBlinking()
+{
+    int randNum = random(10);
+    switch(randNum)
+    {
+        case 0:
+            blink1();
+            break;
+        case 1:
+            blink2();
+            break;
+        case 2:
+            blink3();
+            break;
+        case 3:
+            blink4();
+            break;
+        case 4:
+            blink5();
+            break;
+        case 5:
+            blink6();
+            break;
+        case 6:
+            blink7();
+            break;
+        case 7:
+            blink8();
+            break;
+        case 8:
+            blink9();
+            break;
+        case 9:
+            blink10();
+            break;
+        default:
+            break;
+    }
+}
+    
+void blink1()
+{
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink2()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink3()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink4()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, HIGH);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink5()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, HIGH);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink6()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, HIGH);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink7()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, HIGH);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink8()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, HIGH);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, LOW);
+}
+void blink9()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, HIGH);
+    digitalWrite(LED10, LOW);
+}
+void blink10()
+{
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED7, LOW);
+    digitalWrite(LED8, LOW);
+    digitalWrite(LED9, LOW);
+    digitalWrite(LED10, HIGH);
+}
